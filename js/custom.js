be836f0fa22223f4121156902daec956fc68a7ea
Andrey Omelchuk <andy.omelchuk@gmail.com>
2018-11-11 00:46:18 +0200

    5
    5

--- js/custom.js
+++ js/custom.js
@@ -52,7 +52,7 @@ $(function() {
 		slidesToScroll: 1,
 		dots:true,
 		autoplay: true,
-		autoplaySpeed: 2000,
+		autoplaySpeed: 2000
 	});
 	
 	/*Mask*/
@@ -60,6 +60,42 @@ $(function() {
 		$('input[name="phone"]').mask('+38 0(99) 999-99-99');
 		$('button').on('click', function(event) {
 			event.preventDefault();
+		});
+	});
+	
+	/*FORM*/
+	$.ajaxSetup({
+		url: 'http://ДОМЕН/controller/controller.php',
+		type: 'POST',
+		dataType: 'json',
+		beforeSend: function(){ 
+			console.debug('Спасибо. Форма отправлена');
+		},
+		error: function(req, text, error){
+			console.error('Что-то пошло не так: ' + text + ' | ' + error);
+		},
+		complete: function(){
+			console.debug('Запрос полностью завершен!');
+		}
+	});
+	
+	$(function(){
+		var currentId = $(this).attr("id");
+		console.log(currentId);
+		$(currentId).on('submit', function(e){
+			e.preventDefault();
+			var $that = $(this),
+			formData = new FormData($that.get(0));
+			$.ajax({
+				contentType: false,
+				processData: false,
+				data: formData,
+				success: function(json){
+					if(json){
+						//here we can some thing do with answer
+					}
+				}
+			});
 		});
 	});
 
